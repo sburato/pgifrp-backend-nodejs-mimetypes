790fe2c51278ca19d2d59d89e0a5e58d69d0eda5
Silvano Burato <silvanoburato@gmail.com>
2021-03-11 00:42:32 -0300

    Adicionando comentários de exemplos

--- server.js
+++ server.js
@@ -18,7 +18,10 @@ const mimeTypes = {
 
 const server = http.createServer((request, response) => {      
   let acesso_uri = url.parse(request.url).pathname;  
+  // 127.0.0.1:3000/index.html => acesso_uri = index.html
+
   let caminho_completo_recurso = path.join(process.cwd(), decodeURI(acesso_uri));
+  //process.cwd() => C:\servidor\ + recurso
   console.log(caminho_completo_recurso);
   
   let recurso_carregado;
@@ -31,7 +34,19 @@ const server = http.createServer((request, response) => {
   }
 
   if (recurso_carregado.isFile()) {
-    let mimeType = mimeTypes[path.extname(caminho_completo_recurso).substring(1)];    
+    let extensao = path.extname(caminho_completo_recurso).substring(1);
+    // extname = .html
+    // substring = html
+
+    let mimeType = mimeTypes[extensao];    
+    // html: 'text/html',
+    // css : 'text/css',
+    // js  : 'text/javascript',
+    // png : 'image/png',
+    // jpeg: 'image/jpeg',
+    // jpg : 'image/jpg',
+    // woff: 'font/woof',
+
     response.writeHead(200, {'Content-Type': mimeType});
     let fluxo_arquivo = fs.createReadStream(caminho_completo_recurso);
     fluxo_arquivo.pipe(response);
